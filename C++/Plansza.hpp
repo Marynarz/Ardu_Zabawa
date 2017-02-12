dc5cee7155753d06ada706a52ca3ea7056fd45fa
Wojtek Niedzielski <nwojtek@gmail.com>
2017-02-12 20:38:06 +0100

    Dodawanie punktu

--- C++/Plansza.hpp
+++ C++/Plansza.hpp
@@ -15,6 +15,7 @@ public:
     Plansza(unsigned const int x =3, unsigned const int y =3);
     ~Plansza();
     void showPlansza();
+    void setPoint(unsigned const int x1, unsigned const int y1);
 };
 
 #endif
--- C++/main.cpp
+++ C++/main.cpp
@@ -7,5 +7,8 @@ int main()
 {
     Plansza p1(5,5);
     p1.showPlansza();
+    p1.setPoint(0,1);
+    cout <<endl<<endl;
+    p1.showPlansza();
     return 0;
 }
